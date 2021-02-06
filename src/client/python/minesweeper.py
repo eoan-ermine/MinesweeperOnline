98ba29ad1e3b00f3b7ca3e305e8e25b6d04f98e0
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-06 11:01:03 +0500

    Fix field filling

--- src/client/python/minesweeper.py
+++ src/client/python/minesweeper.py
@@ -1,5 +1,6 @@
-from utils import FieldDescription, SquareContent, Square,\
+from utils import FieldDescription, SquareContent, Square, \
     GameState
+import random
 
 
 class Minesweeper:
@@ -13,26 +14,33 @@ class Minesweeper:
             for dy in range(-1, 2):
                 if dx == 0 and dy == 0:
                     continue
-                if height > x >= 0 and width > y >= 0:
+                if width > x + dx >= 0 and height > y + dy >= 0:
                     neighbourhood.append((x + dx, y + dy))
         return [self.field[i][j] for i, j in neighbourhood]
 
     def initialize_field(self):
-        self.field = [[Square(SquareContent.NONE, False) for _ in range(self.description.width)]
-                      for _ in range(self.description.height)]
+        self.field = [[Square(i, j, SquareContent.NONE, False, "E") for i in range(self.description.width)]
+                      for j in range(self.description.height)]
 
     def initialize_cells(self):
         positions = [(i, j) for i in range(self.description.width) for j in range(self.description.height)]
         mines = random.sample(positions, self.description.mines)
 
         for x, y in mines:
-            self.field[x][y] = Square(SquareContent.MINE, False, "*")
+            self.field[x][y] = Square(x, y, SquareContent.MINE, False, "*")
 
             for cell_x, cell_y in self.get_neighbourhood(x, y):
-                cell = self.field[cell_x][cell_y]
+                cell = self.field[cell_y][cell_x]
                 if cell.content == SquareContent.NONE:
                     cell.content = SquareContent.EMPTY
-                    cell.text = str(int(cell.text) + 1) if cell.text else "0"
+                    cell.text = "1"
+                elif cell.content == SquareContent.EMPTY:
+                    cell.text = str(int(cell.text) + 1)
+
+    def open_cells(self, predicate, cells):
+        for e in cells:
+            if predicate(e):
+                self.open_cell(e.x, e.y)
 
     def open_cell(self, x, y):
         cell = self.field[x][y]
@@ -44,12 +52,13 @@ class Minesweeper:
         if content == SquareContent.EMPTY:
             cell.visible = True
             self.open_cells(filter(
-                lambda e: e.content == SquareContent.EMPTY,
+                lambda e: e.content == SquareContent.EMPTY and not e.visible,
                 self.get_neighbourhood(x, y)
             ))
         elif content == SquareContent.MINE:
+            cell.visible = True
             self.open_cells(filter(
-                lambda e: e.content == SquareContent.MINE,
+                lambda e: e.content == SquareContent.MINE and not e.visible,
                 self.field
             ))
 
--- src/client/python/utils.py
+++ src/client/python/utils.py
@@ -15,12 +15,39 @@ class SquareContent:
     MINE_FLAG = 3,
     QUESTION_FLAG = 4
 
+    def __repr__(self):
+        if self == SquareContent.NONE:
+            return "NONE"
+        elif self == SquareContent.MINE:
+            return "MINE"
+        elif self == SquareContent.EMPTY:
+            return "EMPTY"
+        elif self == SquareContent.MINE_FLAG:
+            return "MINE_FLAG"
+        elif self == SquareContent.QUESTION_FLAG:
+            return "QUESTION_FLAG"
+
+    def __str__(self):
+        return self.__repr__()
+
 
 class Square:
-    def __init__(self, content: SquareContent, visible: bool, text: str = ""):
+    def __init__(self, x, y, content: SquareContent, visible: bool, text: str = ""):
+        self.x = x
+        self.y = y
+
         self.content = content
         self.text = text
         self.visible = visible
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def __str__(self) -> str:
+        return self.text
+
+    def __repr__(self) -> str:
+        return f"Square({self.x}, {self.y}, {self.content.__str__()}, {self.text}, {self.visible})"
 
 
 class GameState:
