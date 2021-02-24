97a83e2a2a77f4bfd201d096d81d582203c589da
PatriotRossii <patriotrossii2019@mail.ru>
2021-02-24 10:10:46 +0500

    Implement edit playlist dialog

--- src/client/python/dialogues/edit_playlist.py
+++ src/client/python/dialogues/edit_playlist.py
@@ -20,6 +20,8 @@ def open_playlist_dialog(game):
 class EditPlaylistDialog(QDialog):
     def __init__(self, game, parent=None):
         super().__init__(parent)
+
+        self.game = game
         self.music_subsystem = game.get_music_subsystem()
 
         self.setAttribute(Qt.WA_DeleteOnClose)
@@ -29,8 +31,46 @@ class EditPlaylistDialog(QDialog):
         self.init_ui()
         self.init_signals()
 
+    def load_playlist(self):
+        self.list_widget.clear()
+        self.list_widget.addItems([
+            *[e.filename for e in self.music_subsystem.sounds]
+        ])
+
+    def open_add_dialog(self):
+        dialog = AddTrackDialog(self.game)
+        dialog.exec_()
+        self.load_playlist()
+
+    def delete_music_track(self):
+        current_item = self.list_widget.currentItem()
+        if current_item:
+            self.music_subsystem.remove(current_item.text())
+        self.load_playlist()
+
     def init_ui(self):
-        pass
+        self.load_playlist()
 
     def init_signals(self):
-        pass
+        self.add_btn.clicked.connect(self.open_add_dialog)
+        self.remove_btn.clicked.connect(self.delete_music_track)
+
+
+class AddTrackDialog(QDialog):
+    def __init__(self, game, parent=None):
+        super().__init__(parent)
+        self.music_subsystem = game.get_music_subsystem()
+
+        self.setAttribute(Qt.WA_DeleteOnClose)
+
+        uic.loadUi("dialogues/forms/add_track_dialog.ui", self)
+
+        self.init_signals()
+
+    def add_music_track(self):
+        self.music_subsystem.add(
+            self.lineEdit.text(), ""
+        )
+
+    def init_signals(self):
+        self.buttonBox.accepted.connect(self.add_music_track)
--- src/client/python/music/music.py
+++ src/client/python/music/music.py
@@ -3,6 +3,16 @@ import pygame
 MUSIC_END_EVENT = pygame.USEREVENT + 1
 
 
+def load_file(filename: str, prefix: str = "resources/music/") -> pygame.mixer.Sound:
+    return pygame.mixer.Sound(prefix + filename)
+
+
+class MusicTrack:
+    def __init__(self, filename, prefix: str):
+        self.filename = filename
+        self.sound = load_file(filename, prefix)
+
+
 class MusicSubsystem:
     def __init__(self, *args):
         pygame.mixer.init()
@@ -11,21 +21,20 @@ class MusicSubsystem:
         pygame.mixer.music.set_endevent(MUSIC_END_EVENT)
 
         self.sounds = [
-            self.load_file(filename) for filename in args
+            MusicTrack(filename, "resources/music/") for filename in args
         ]
         self.current_sound = -1
         self.length_of_playlist = len(self.sounds)
 
         self.queue_next()
 
-    @staticmethod
-    def load_file(filename: str) -> pygame.mixer.Sound:
-        return pygame.mixer.Sound("resources/music/" + filename)
-
-    def add(self, filename):
-        self.sounds.append(self.load_file(filename))
+    def add(self, filename, prefix):
+        self.sounds.append(MusicTrack(filename, prefix))
         self.length_of_playlist += 1
+
+    def remove(self, filename):
+        self.sounds = [e for e in self.sounds if e.filename != filename]
 
     def queue_next(self):
         self.current_sound = (self.current_sound + 1) % self.length_of_playlist
-        self.playlist.queue(self.sounds[self.current_sound])
+        self.playlist.queue(self.sounds[self.current_sound].sound)
